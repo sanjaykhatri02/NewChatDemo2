4e9ba60dc9128e23bae8e6a7e2fe83f19541cac5
Sanjay Khatri <sanjay.khatri02@outlook.com>
2024-09-24 13:00:12 +0500

    Initial Commit

---
+++ TestChatNew.framework/Headers/TestChatNew.h
@@ -0,0 +1,18 @@
+//
+//  TestChatNew.h
+//  TestChatNew
+//
+//  Created by Sanjay Kumar on 24/09/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestChatNew.
+FOUNDATION_EXPORT double TestChatNewVersionNumber;
+
+//! Project version string for TestChatNew.
+FOUNDATION_EXPORT const unsigned char TestChatNewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestChatNew/PublicHeader.h>
+
+
