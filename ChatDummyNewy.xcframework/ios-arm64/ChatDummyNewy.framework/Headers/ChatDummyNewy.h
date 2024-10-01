6cda41679bd5aa13d154d3640fde300f20acb613
Sanjay Khatri <sanjay.khatri02@outlook.com>
2024-10-01 16:16:39 +0500

    Initial Commit

---
+++ ChatDummyNewy.xcframework/ios-arm64/ChatDummyNewy.framework/Headers/ChatDummyNewy.h
@@ -0,0 +1,18 @@
+//
+//  ChatDummyNewy.h
+//  ChatDummyNewy
+//
+//  Created by Sanjay Kumar on 01/10/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChatDummyNewy.
+FOUNDATION_EXPORT double ChatDummyNewyVersionNumber;
+
+//! Project version string for ChatDummyNewy.
+FOUNDATION_EXPORT const unsigned char ChatDummyNewyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChatDummyNewy/PublicHeader.h>
+
+
